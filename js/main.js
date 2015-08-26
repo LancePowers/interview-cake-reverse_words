1a0f5619cc915349d25cf502cc7ed3e72c53146f
Lance <Lance@thelpstory.com>
2015-08-26 10:58:06 -0600

    finished

---
+++ js/main.js
@@ -0,0 +1,5 @@
+var message = 'find you will pain only go you recordings security the into if'
+console.log(reverse_words(message));
+function reverse_words(message){
+  return message.split(' ').reverse().join(' ');
+}
